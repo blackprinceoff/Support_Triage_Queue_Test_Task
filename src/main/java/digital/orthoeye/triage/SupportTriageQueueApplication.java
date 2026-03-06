c1a26159b0ddb4e00efe528f2e33a77d0eafd8a4
blackprinceoff <myroslav.moskva.pm.2022@lpnu.ua>
2026-03-06 22:55:07 +0200

    chore: initial Spring Boot project setup

---
+++ src/main/java/digital/orthoeye/triage/SupportTriageQueueApplication.java
@@ -0,0 +1,13 @@
+package digital.orthoeye.triage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportTriageQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportTriageQueueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/digital/orthoeye/triage/SupportTriageQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package digital.orthoeye.triage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportTriageQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
